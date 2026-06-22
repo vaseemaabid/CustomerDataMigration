b7034303e5dcd4130f991379ce0a13c8e9d9eedd
vaseemaabid <154531298+vaseemaabid@users.noreply.github.com>
2026-06-22 15:38:48 +0530

    Create customer_migration database and customers table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,10 @@
+CREATE DATABASE customer_migration;
 
+USE customer_migration;
+
+CREATE TABLE customers (
+    customer_id INT,
+    name VARCHAR(50),
+    email VARCHAR(100),
+    phone VARCHAR(15)
+);
